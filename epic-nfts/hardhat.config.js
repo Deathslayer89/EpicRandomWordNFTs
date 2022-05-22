1fd93bcc46b4840e34b6d19ae3f5732f33a1bf95
Deathslayer89 <Dineshswain2001@gmail.com>
2022-05-22 19:35:26 +0530

    contract update

--- epic-nfts/hardhat.config.js
+++ epic-nfts/hardhat.config.js
@@ -1,18 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
-require("dotenv").config({path:".env"});
+require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+const ETHERSCAN_API_KEY=process.env.ETHERSCAN_API_KEY;
 module.exports = {
-  solidity: "0.8.1",
-  networks:{
-    rinkeby:{
-      url:process.env.ALCHEMY_API_KEY_URL,
-      accounts:[process.env.RINKEBY_PRIVATE_KEY],
-    }
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_KEY_URL,
+      accounts: [process.env.RINKEBY_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apikey:{
+     rinkeby:ETHERSCAN_API_KEY,
+    },
   }
 };
